58a034606205af900b60c4cfac6b4ad029764e5f
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-09 04:33:39 +0000

    implemented for loop to parse through data, sent hardcoded data to server for test

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,11 +1,17 @@
+var bodyPasrser = require('body-parser');
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+var urlencodedParser = bodyPasrser.urlencoded({extended:false});
+
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
     
-    app.post('/todo', function(req, res){
-        
+    app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json(data)
     });
     
     app.delete('/todo', function(req, res){
